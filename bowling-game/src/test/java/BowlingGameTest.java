3bffca0a5e6568c506861b9898134912d96742e8
Adam Kosofsky <akosofsky@ad.avenuelink.com>
2024-10-31 10:50:20 -0500

    Add an entry point to the app, example test, and set java to 21

---
+++ bowling-game/src/test/java/BowlingGameTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BowlingGameTest {
+
+  @Test
+  public void testExample() {
+    Assertions.assertEquals("hello", "hello");
+  }
+
+}
